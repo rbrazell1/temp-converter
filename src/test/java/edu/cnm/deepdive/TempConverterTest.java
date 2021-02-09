35be4ca9169d14683f4fc99ea967e3c96a964107
rbrazell1 <brazellrussell@gmail.com>
2021-02-09 14:22:08 -0700

    set up test case in 2d array

--- src/test/java/edu/cnm/deepdive/TempConverterTest.java
+++ src/test/java/edu/cnm/deepdive/TempConverterTest.java
@@ -8,29 +8,33 @@ class TempConverterTest {
 
   static final float TOLERANCE_THOUSANDTHS = .001f;
   static final float TOLERANCE_TEN_THOUSANDTHS = .0018f;
-  static final float[] celsius = {-40, 0, 100};
-  static final float[] fahrenheit = {-40, 32, 212};
+  static final float[][] testCases = {
+      // {celsius, fahrenheit}
+      {-40, -40},
+      {0, 32},
+      {100, 212},
+  };
 
   @Test
   void convertFToC() {
-    for (int i = 0; i < celsius.length; i++){
-      float expected = celsius[i];
-      float actual = TempConverter.convertFToC(fahrenheit[i]);
+    for (float [] testCase : testCases){
+      float expected = testCase[0];
+      float input = testCase[1];
+      float actual = TempConverter.convertFToC(input);
       System.out.printf("Expected Celsius = %.3f; input fahrenheit =%.3f; actual celsius = %.3f%n;",
-          expected, fahrenheit[i], actual);
+          expected, testCase[1], actual);
       assertEquals(expected, actual, TOLERANCE_THOUSANDTHS);
     }
    }
 
   @Test
   void convertCToF() {
-    for (int i = 0; i < fahrenheit.length; i++){
-      float expected = fahrenheit[i];
-      float actual = TempConverter.convertCToF(celsius[i]);
-      System.out.printf("Input celsius =%.3f; Expected fahrenheit =%.3f; Actual fahrenheit =%.3f%n;",
-          celsius[i], expected, actual);
+    for (float [] testCase : testCases){
+      float expected = testCase[1];
+      float input = testCase[0];
+      float actual = TempConverter.convertCToF(input);
+      System.out.printf("Expected Celsius = %.3f; input fahrenheit =%.3f; actual celsius = %.3f%n;",
+          expected, testCase[0], actual);
       assertEquals(expected, actual, TOLERANCE_TEN_THOUSANDTHS);
     }
   }
-
-}
